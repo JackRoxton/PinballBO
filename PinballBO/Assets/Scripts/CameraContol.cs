b53f36b9ee99417c71009215fed2960cd0934e6f
QuentinLamy <quentin.lamy16@gmail.com>
2021-03-04 17:23:19 +0100

    I tried

---
+++ PinballBO/Assets/Scripts/CameraContol.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraContol : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+}
