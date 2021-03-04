5eeb6e65baedd90f3d4208d228445feab612cb91
Quaquoum <55735086+Quaquoum@users.noreply.github.com>
2021-03-04 16:41:08 +0100

    Timer

---
+++ PinballBO/Assets/Scripts/Timer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    //Filling
+    [Range(0, 100)]
+    public float fillValue = 50;
+    public Image circleFillImage;
+
+    //Timer
+    public float timeTotal = 30f;
+    private float timeLeft;
+
+    private void Start()
+    {
+        timeLeft = timeTotal;
+    }
+
+    void Update()
+    {
+        if (timeLeft < 0)
+        {
+            //Game Over
+        }
+        else
+        {
+            if (timeLeft > timeTotal)
+            {
+                timeLeft = timeTotal;
+            }
+
+            fillValue = timeLeft / timeTotal * 100;
+            Debug.Log(fillValue);
+
+            timeLeft -= Time.deltaTime;
+            FillCircleValue(fillValue);
+        }
+    }
+
+    void FillCircleValue(float value)
+    {
+        float fillAmount = (value / 100.0f);
+        circleFillImage.fillAmount = fillAmount;
+    }
+
+}
