f81737d59320efd3838acb30c58e4c8f114fd9a0
oct0punk <elric.emile@gmail.com>
2021-03-04 16:35:57 +0100

    Bumper Rond

---
+++ PinballBO/Assets/Scripts/Bumper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    public int force;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        Debug.Log("Collision");
+        Bill bill = collision.collider.GetComponent<Bill>();
+        if (bill != null)
+        {
+            Debug.Log("Collision with Bill");
+            Vector3 direction = bill.transform.position - transform.position;
+            bill.GetComponent<Rigidbody>().AddForce(direction * force);
+        }
+    }
+}
