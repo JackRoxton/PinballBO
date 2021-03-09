eceadd5abf8d9fa0042aef777c0efa3cc3b34b1f
QuentinLamy <quentin.lamy16@gmail.com>
2021-03-09 11:57:31 +0100

    caméra fixe

--- PinballBO/Assets/Scripts/CameraContol.cs
+++ PinballBO/Assets/Scripts/CameraContol.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class CameraContol : MonoBehaviour
 {
-    private void Start()
-    {
+    [SerializeField]
+    Transform player;
 
-    }
+    Vector3 CameraGap = new Vector3(0, 1, -6);
+
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        this.transform.position = player.position + CameraGap;
     }
 }
