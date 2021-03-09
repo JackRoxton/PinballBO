18e55808ce865925ef1666037ebba456c1932cc0
QuentinLamy <quentin.lamy16@gmail.com>
2021-03-09 14:04:39 +0100

    wip cam + prefab

--- PinballBO/Assets/Scripts/CameraContol.cs
+++ PinballBO/Assets/Scripts/CameraContol.cs
@@ -9,13 +9,31 @@ public class CameraContol : MonoBehaviour
 
     Vector3 CameraGap = new Vector3(0, 1, -6);
 
+    /*float mouseSensitivity = 75f;
+    Vector3 angle;*/
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     void Update()
     {
         Move();
+        //Advanced();
     }
 
     void Move()
     {
         this.transform.position = player.position + CameraGap;
     }
+
+    /*void Advanced() //un WIP d'une caméra qui tourne autour de la bille si le besoin est
+    {
+        angle.x += -Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        angle.y += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        Quaternion lookRotation = Quaternion.Euler(angle);
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, 0.2f);
+    }*/
 }
