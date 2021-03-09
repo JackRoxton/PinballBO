1ebc87458af9dcf6a7502da0806cd33df5766574
Quaquoum <55735086+Quaquoum@users.noreply.github.com>
2021-03-09 15:06:58 +0100

    Ecran défaite, prototype

---
+++ PinballBO/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                Debug.LogError("GameManager instance not found");
+
+            return instance;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
