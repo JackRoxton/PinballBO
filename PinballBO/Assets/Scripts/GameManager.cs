cc88156321931c7b6008ccb85c99e6a666b3f797
Quaquoum <55735086+Quaquoum@users.noreply.github.com>
2021-03-09 15:50:19 +0100

    Main Menu Scene

--- PinballBO/Assets/Scripts/GameManager.cs
+++ PinballBO/Assets/Scripts/GameManager.cs
@@ -4,29 +4,6 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private static GameManager instance;
-    public static GameManager Instance
-    {
-        get
-        {
-            if (instance == null)
-                Debug.LogError("GameManager instance not found");
-
-            return instance;
-        }
-    }
-
-    public void RestartGame()
-    {
-        
-    }
-
-    public void QuitGame()
-    {
-        Debug.Log("Has quit game");
-        Application.Quit();
-    }
-
     public void GameOver()
     {
 
