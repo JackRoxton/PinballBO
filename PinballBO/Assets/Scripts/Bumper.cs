f42397fe85cea4bb7efeec7f32926eab1d4b72a9
oct0punk <elric.emile@gmail.com>
2021-03-11 14:56:18 +0100

    AudioManager

--- PinballBO/Assets/Scripts/Bumper.cs
+++ PinballBO/Assets/Scripts/Bumper.cs
@@ -6,13 +6,20 @@ public class Bumper : MonoBehaviour
 {
     public int force;
 
+    AudioSource source;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         Bill bill = collision.collider.GetComponent<Bill>();
         if (bill != null)
         {
             Vector3 direction = collision.GetContact(0).normal;
-            Debug.Log(direction);
+            AudioManager.Instance.PlayClip(source, source.clip);
             bill.GetComponent<Rigidbody>().AddForce(-direction * force);
         }
     }
