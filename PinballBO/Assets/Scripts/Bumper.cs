a552496f0ac35a4812cc058ab75ab407ce1870b5
oct0punk <elric.emile@gmail.com>
2021-03-16 16:25:43 +0100

    Bumper final sans textures

--- PinballBO/Assets/Scripts/Bumper.cs
+++ PinballBO/Assets/Scripts/Bumper.cs
@@ -8,22 +8,53 @@ public class Bumper : MonoBehaviour
     public int factor;
 
     AudioSource source;
+    Animator animator;
+
+    private bool isTrigger = false;
 
     private void Awake()
     {
         source = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+
+        foreach (MeshCollider meshCollider in GetComponentsInChildren<MeshCollider>())
+        {
+            if (meshCollider.isTrigger)
+                isTrigger = true;
+            return;
+        }
     }
 
+    // SlingShots   ( triangle bumper )
     private void OnCollisionEnter(Collision collision)
     {
+        if (isTrigger) return;
+
         Bill bill = collision.collider.GetComponent<Bill>();
         if (bill != null)
         {
             Vector3 direction = collision.GetContact(0).normal;
             AudioManager.Instance.PlayClip(source, source.clip);
             bill.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            Debug.Log(bill.GetComponent<Rigidbody>().velocity.magnitude);
             bill.GetComponent<Rigidbody>().AddForce(-direction * (force + bill.GetComponent<Rigidbody>().velocity.magnitude * factor));
+        }
+    }
+
+    // Classic Bumper
+    private void OnTriggerEnter(Collider other)
+    {
+        Bill bill = other.GetComponent<Bill>();
+        if (bill != null)
+        {
+            Rigidbody billBody = bill.GetComponent<Rigidbody>();
+            Vector3 direction = (bill.transform.position - transform.position).normalized;
+            direction = new Vector3(direction.x, 0, direction.z);
+
+            Debug.Log("Bump");
+            AudioManager.Instance.PlayClip(source, source.clip);
+            animator.SetTrigger("Bump");
+            billBody.velocity = Vector3.zero;
+            billBody.AddForce(direction * (force + billBody.velocity.magnitude * factor));
         }
     }
 }
