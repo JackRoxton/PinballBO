3b6dab29d5fb9aae20c7ff4c9a3095111b77cc8c
QuentinLamy <quentin.lamy16@gmail.com>
2021-03-25 17:27:40 +0100

    porte (caméra bugguée)

---
+++ PinballBO/Assets/Scripts/Items/Porte.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Porte : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera camera;
+
+    [SerializeField]
+    List<Targets> targets = new List<Targets>();
+
+    [SerializeField]
+    int offTargetsCount = 0;
+
+    Vector3 pos;
+    void Start()
+    {
+        pos = new Vector3(this.transform.localPosition.x - 2, this.transform.localPosition.y, this.transform.localPosition.z);
+        for (int i = 0; i < offTargetsCount; i++)
+        {
+            targets[Random.Range(0, targets.Count)].SetLights(false);
+        }
+    }
+
+    void Update()
+    {
+        if (testLights())
+        {
+            StartCoroutine(Open());
+        }
+    }
+
+    bool testLights()
+    {
+        foreach (Targets n in targets)
+        {
+            if(!n.SendLights())
+            {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    IEnumerator Open()
+    {
+        CameraManager.Instance.SetCameraActive(camera.gameObject);
+        yield return new WaitForSeconds(2);
+        this.transform.localPosition = Vector3.MoveTowards(this.transform.localPosition, pos, 0.05f);
+        yield return new WaitForSeconds(2);
+        CameraManager.Instance.SetCameraActive(CameraManager.Instance.mainCam.gameObject);
+    }
+}
---
+++ PinballBO/Assets/Scripts/Items/Targets.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targets : MonoBehaviour
+{
+    private bool lightState = true;
+
+    public void SetLights(bool state)
+    {
+        this.lightState = state;
+    }
+
+    public bool SendLights()
+    {
+        return this.lightState;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Bill bill = collision.gameObject.GetComponent<Bill>();
+
+        if(bill != null && !this.lightState)
+        {
+            this.lightState = true;
+        }
+    }
+}
