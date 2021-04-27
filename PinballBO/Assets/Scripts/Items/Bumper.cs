a778e89f191410d5a4e38cd49ec002838a7c6f2b
Quaquoum <55735086+Quaquoum@users.noreply.github.com>
2021-04-27 15:10:30 +0200

    Particles Bumpers

--- PinballBO/Assets/Scripts/Items/Bumper.cs
+++ PinballBO/Assets/Scripts/Items/Bumper.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class Bumper : BumpObject
 {
+    [SerializeField] private ParticleSystem bumpParticle;
+    private Vector3 particlePos;
 
     // Classic Bumper
     private void OnTriggerEnter(Collider other)
     {
+        Vector3 particlePos = new Vector3(this.transform.position.x, this.transform.position.y + 1.5f, this.transform.position.z);
         Bill bill = other.GetComponent<Bill>();
         if (bill != null)
         {
             BumpAway(bill, "Bump");
+            Instantiate(bumpParticle, particlePos, Quaternion.identity);
         }
     }
 }
