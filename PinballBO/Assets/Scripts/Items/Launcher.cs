e7184cd0c751bdd162f68ca280ce2a7df4bc297d
oct0punk <elric.emile@gmail.com>
2021-04-29 14:13:20 +0200

    LanceBill

--- PinballBO/Assets/Scripts/Items/Launcher.cs
+++ PinballBO/Assets/Scripts/Items/Launcher.cs
@@ -7,41 +7,34 @@ public class Launcher : MonoBehaviour
     [SerializeField] private int force;
     Animator animator;
     bool isLaunching = false;
-    public BoxCollider collider;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
     }
 
-    IEnumerator Launch()
+    IEnumerator Launch(Bill bill)
     {
         isLaunching = true;
         animator.SetTrigger("Launch");
         yield return new WaitForSeconds(1);
-        isLaunching = false;
-        collider.enabled = true;
+        yield return new WaitForSeconds(.2f);
+        bill.GetComponent<Rigidbody>().velocity = transform.right * force;
         yield return new WaitForSeconds(.5f);
-        collider.enabled = false;
+        isLaunching = false;
     }
 
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        Bill bill = collision.collider.GetComponent<Bill>();
-        if (bill != null)
-        {
-            if (!isLaunching)
-                StartCoroutine(Launch());
-        }
-    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (isLaunching)
-            if (other.GetComponent<Bill>() != null)
+        if (!isLaunching)
+        {
+            Bill bill = other.GetComponent<Bill>();
+            if (bill != null)
             {
-                other.GetComponent<Rigidbody>().velocity = transform.right * force;
+                StartCoroutine(Launch(bill));
             }
+        }
     }
 }
