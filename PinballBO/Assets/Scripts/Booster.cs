7efa6390d95164003ed80d10b4c25736cd3005f6
QuentinLamy <quentin.lamy16@gmail.com>
2021-05-04 14:53:36 +0200

    Booster

---
+++ PinballBO/Assets/Scripts/Booster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Booster : MonoBehaviour
+{
+    [SerializeField]
+    private float boosterForce;
+
+    private void OnTriggerStay(Collider other)
+    {
+        Bill bill = other.gameObject.GetComponent<Bill>();
+        if(bill != null)
+        {
+            Boost(bill);
+        }
+    }
+
+    void Boost(Bill bill)
+    {
+        bill.gameObject.GetComponent<Rigidbody>().AddForce(this.transform.localRotation * Vector3.forward * boosterForce,ForceMode.Acceleration);
+    }
+}
