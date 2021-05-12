9019b64eadf66af74f915dc21a7126a20871f359
QuentinLamy <quentin.lamy16@gmail.com>
2021-05-12 15:09:32 +0200

    boue au sol (création par les particules à faire)

---
+++ PinballBO/Assets/Scripts/Mud.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mud : MonoBehaviour
+{
+    [SerializeField,Range(0,25)]
+    private float dragStrength;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Bill bill = other.gameObject.GetComponent<Bill>();
+        if(bill != null)
+        {
+            bill.gameObject.GetComponent<Rigidbody>().drag += dragStrength;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Bill bill = other.gameObject.GetComponent<Bill>();
+        if (bill != null)
+        {
+            bill.gameObject.GetComponent<Rigidbody>().drag -= dragStrength;
+        }
+    }
+}
