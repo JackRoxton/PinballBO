4267569f4d1cf7db07947325db5343832ad5dc98
QuentinLamy <quentin.lamy16@gmail.com>
2021-06-01 15:49:54 +0200

    test anim boost + fix

---
+++ PinballBO/Assets/Art/Materials/boostAnim.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boostAnim : MonoBehaviour
+{
+    public float scroll = 1f;
+
+    private void Update()
+    {
+        float offset = Time.time * scroll;
+        this.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offset, offset);
+    }
+}
+
+
