68588db0cca0a37c034ea090aef940c760b9cefc
QuentinLamy <quentin.lamy16@gmail.com>
2021-06-03 15:00:33 +0200

    fix + mat

--- PinballBO/Assets/Art/Materials/boost/boostAnim.cs
+++ PinballBO/Assets/Art/Materials/boost/boostAnim.cs
@@ -30,7 +30,6 @@ public class boostAnim : MonoBehaviour
             {
                 o.GetComponent<MeshRenderer>().material = none;
             }
-            Debug.Log("Blinking");
         }
 
 
